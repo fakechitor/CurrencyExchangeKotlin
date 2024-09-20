de628e27f998c65e04dc4fc5ffacc81107e0448f
fakechitor <babaykin192@gmail.com>
2024-09-20 16:30:24 +0300

    1st commit

---
+++ src/main/kotlin/main/kotlin/currencyexchangekotlin/HelloApplication.kt
@@ -0,0 +1,9 @@
+package main.kotlin.currencyexchangekotlin
+
+import jakarta.ws.rs.ApplicationPath
+import jakarta.ws.rs.core.Application
+
+@ApplicationPath("/api")
+class HelloApplication : Application() {
+
+}
