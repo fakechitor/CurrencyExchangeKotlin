76bae5a895ecde1a544563c0b6b9cb1ef265e4f7
fakechitor <ladasedanbaklajan06@gmail.com>
2024-10-11 00:31:27 +0300

    fix: delete unused classes

--- src/main/kotlin/main/kotlin/currencyexchange/HelloApplication.kt
+++
@@ -1,9 +0,0 @@
-package main.kotlin.currencyexchange
-
-import jakarta.ws.rs.ApplicationPath
-import jakarta.ws.rs.core.Application
-
-@ApplicationPath("/api")
-class HelloApplication : Application() {
-
-}
