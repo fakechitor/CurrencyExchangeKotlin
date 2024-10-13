e043f52c7c4ad23bcde37ff33b39cb9114cb6426
fakechitor <ladasedanbaklajan06@gmail.com>
2024-10-13 17:23:06 +0300

    feat: added http 500 to GET and 409 to POST

---
+++ src/main/kotlin/main/kotlin/currencyexchange/servlets/CurrencyServlet.kt
@@ -0,0 +1,44 @@
+package main.kotlin.currencyexchange.servlets
+
+import com.google.gson.Gson
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import main.kotlin.currencyexchange.data.dao.CurrencyDAO
+import main.kotlin.currencyexchange.dto.CurrencyDTO
+
+@WebServlet(name = "getCurrency", value = ["/currency/*"])
+class CurrencyServlet : HttpServlet() {
+    private val currencyDAO = CurrencyDAO()
+
+    override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        val pathInfo = req.pathInfo?.split("/") ?: listOf()
+        val code = if (pathInfo.size > 1) pathInfo[1] else ""
+        try{
+            if (code != "") {
+                val currency = currencyDAO.getByCode(code)
+                resp.contentType = "application/json"
+                val printWriter = resp.writer
+                if (currency != null) {
+                    val currencyDTO = CurrencyDTO(
+                        id = currency.id,
+                        currencyCode = currency.currencyCode,
+                        name = currency.name,
+                        sign = currency.sign,
+                    )
+                    val jsonResponse = Gson().toJson(currencyDTO)
+                    printWriter.write(jsonResponse)
+                } else {
+                    resp.status = HttpServletResponse.SC_NOT_FOUND
+                }
+            } else {
+                resp.status = HttpServletResponse.SC_BAD_REQUEST
+            }
+        }
+        catch (e: Exception){
+            resp.status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR
+        }
+    }
+
+}
