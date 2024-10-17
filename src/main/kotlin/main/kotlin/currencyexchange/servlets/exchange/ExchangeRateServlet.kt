dd4bd25adbd95a4c2eaed9b3a06e2768a654f70b
fakechitor <ladasedanbaklajan06@gmail.com>
2024-10-17 14:55:10 +0300

    feat: added GET for /exchangeRate

---
+++ src/main/kotlin/main/kotlin/currencyexchange/servlets/exchange/ExchangeRateServlet.kt
@@ -0,0 +1,45 @@
+package main.kotlin.currencyexchange.servlets.exchange
+
+import com.google.gson.Gson
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import main.kotlin.currencyexchange.dto.ExchangeRateDTO
+import main.kotlin.currencyexchange.service.ExchangeService
+
+@WebServlet(name = "exchangeRate", value = ["/exchangeRate/*"])
+class ExchangeRateServlet : HttpServlet() {
+    private val exchangeService = ExchangeService()
+    private val gson = Gson()
+
+
+    override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        val connectedCodes = req.pathInfo.trim('/')
+        try {
+            if (connectedCodes.length == 6) {
+                val exchangeRate = exchangeService.getByCode(connectedCodes)
+                resp.contentType = "application/json"
+                val printWriter = resp.writer
+                val exchangeRateDTO = ExchangeRateDTO(
+                    id = exchangeRate.id,
+                    baseCurrency = exchangeRate.baseCurrency,
+                    targetCurrency = exchangeRate.targetCurrency,
+                    rate = exchangeRate.rate,
+                )
+                val jsonResponse = gson.toJson(exchangeRateDTO)
+                printWriter.write(jsonResponse)
+            } else {
+                resp.status = HttpServletResponse.SC_BAD_REQUEST
+            }
+        }
+        catch (e: IllegalArgumentException) {
+            resp.status = HttpServletResponse.SC_NOT_FOUND
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+            resp.status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR
+        }
+
+    }
+}
