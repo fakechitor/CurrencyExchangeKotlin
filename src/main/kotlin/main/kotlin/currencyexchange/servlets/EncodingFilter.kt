a97b5879acdb09ae6637a0ce983151782936c74f
fakechitor <ladasedanbaklajan06@gmail.com>
2024-10-26 14:42:35 +0300

    feat: implemented CORS to filter

--- src/main/kotlin/main/kotlin/currencyexchange/servlets/EncodingFilter.kt
+++ src/main/kotlin/main/kotlin/currencyexchange/servlets/EncodingFilter.kt
@@ -3,13 +3,13 @@ package main.kotlin.currencyexchange.servlets
 import jakarta.servlet.*
 import jakarta.servlet.annotation.WebFilter
 import java.io.IOException
-
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 
 @WebFilter(urlPatterns = ["/*"])
 class EncodingFilter : Filter {
-    @Throws(ServletException::class)
-    override fun init(filterConfig: FilterConfig?) {
-    }
+
+    override fun init(filterConfig: FilterConfig?) {}
 
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain) {
@@ -17,9 +17,18 @@ class EncodingFilter : Filter {
         servletRequest.characterEncoding = "UTF-8"
         servletResponse.contentType = "application/json"
 
+        val httpResponse = servletResponse as HttpServletResponse
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*")
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+        val httpRequest = servletRequest as HttpServletRequest
+        if ("OPTIONS".equals(httpRequest.method, ignoreCase = true)) {
+            httpResponse.status = HttpServletResponse.SC_OK
+            return
+        }
+
         filterChain.doFilter(servletRequest, servletResponse)
     }
-
-    override fun destroy() {
-    }
+    override fun destroy() {}
 }
